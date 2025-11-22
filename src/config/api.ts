90fd48e1d7ce17d269de15b78a6446b2390ea3c3
Shruti <shruti.pathak@dream11.com>
2025-11-22 23:17:19 +0530

    fix: added env check for uat

--- src/config/api.ts
+++ src/config/api.ts
@@ -2,8 +2,13 @@
  * Determines the base URL based on the environment.
  */
 const getBaseUrl = () => {
-  if (process.env.NODE_ENV === "production") {
+  const env = process.env.NEXT_PUBLIC_ENV || process.env.NODE_ENV;
+
+  if (env === "production") {
     return "https://thunder-master.dream11.local/thunder";
+  }
+  if (env === "uat") {
+    return "https://thunder-uat.dream11.local/thunder";
   }
   return "/thunder";
 };
@@ -11,13 +16,14 @@ const getBaseUrl = () => {
 export const API_BASE_URLS = {
   THUNDER: getBaseUrl(),
   USER_COHORTS: "/user-cohorts",
+  CONCORD: "/concord",
 };
 
 export const API_ENDPOINTS = {
   FILTERS_LIST: `${API_BASE_URLS.THUNDER}/filters`,
   JOURNEYS_LIST: `${API_BASE_URLS.THUNDER}/ctas`,
-  EVENTS_SCHEMA: "/concord/schema",
-  SYSTEM_PROPERTIES: "/concord/getSystemProperties",
+  EVENTS_SCHEMA: `${API_BASE_URLS.CONCORD}/schema`,
+  SYSTEM_PROPERTIES: `${API_BASE_URLS.CONCORD}/getSystemProperties`,
   COHORTS_REALTIME: `${API_BASE_URLS.USER_COHORTS}/user-cohort/realtime`,
 };
 
