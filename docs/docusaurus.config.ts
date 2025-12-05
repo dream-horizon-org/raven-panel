b557688298a5fac5c9e963aeba1a76cf1ec8c785
gsjd11 <ganga.sagar@d11.com>
2025-12-05 19:13:58 +0530

    chore: changing url from raven-panel to raven

--- docs/docusaurus.config.ts
+++ docs/docusaurus.config.ts
@@ -8,7 +8,7 @@ const config: Config = {
   favicon: 'img/favicon.ico',
 
   url: 'https://dream-horizon-org.github.io',
-  baseUrl: '/raven-panel/',
+  baseUrl: '/raven/',
 
   organizationName: 'dream-horizon-org',
   projectName: 'raven-panel',
